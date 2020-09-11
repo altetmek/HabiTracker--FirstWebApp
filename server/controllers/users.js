70c6377f54814b2f6b02a110e5835fcbaab2fc47
Gustav <gustav.skallberg@gmail.com>
2020-09-11 16:56:42 +0200

    Added get by id and delete by id

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -9,12 +9,35 @@ router.get('/', function(req, res, next){
     });
 });
 
+router.get('/:id', function(req, res, next) {
+    var id = req.params.id;
+    User.findById(id, function(err, users) {
+        if (err) { return next(err); }
+        if (users === null) {
+            return res.status(404).json({'message': 'User not found'});
+        }
+        res.json(users);
+    });
+});
+
 router.post('/', function(req, res, next) {
     var user = new User(req.body);
     user.save(function(err, user) {
         if(err) { return next(err); }
         res.status(201).json(user);
     });
+});
+
+router.delete('/:id', function(req, res, next) {
+    var id = req.params.id;
+    User.findOneAndDelete({_id: id}, function(err, user) {
+        if (err) {return next(err); }
+        if (user === null){
+            return res.status(404).json({'message': 'User not found'});
+        }
+        res.json(user)
+    
+        });
 });
 
 module.exports = router;
