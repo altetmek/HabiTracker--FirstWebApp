54cdad130360241d7be2eccf6503e28f753fae58
Altug <gusaltetal@student.gu.se>
2020-09-14 17:41:50 +0200

    Fixed the issue with get for user

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -5,6 +5,9 @@ var User = require('../models/user');
 router.get('/', function(req, res, next){
     User.find(function(err, users){
         if (err) {return next(err);}
+        if (users.length === 0) {
+            return res.status(404).json({'message': 'There is no existing user!'});
+        }
         res.json({'users': users});
     });
 });
