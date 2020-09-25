6ccd23250a83250d57e7b3aab16526801223b3b7
Filip <filiple@student.chalmers.se>
2020-09-25 13:36:03 +0200

    UserPage added

---
+++ client/src/views/UserPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-container class="page-container" fluid="lg">
+        <b-row>
+         <b-col>1 of 3</b-col>
+            <b-col>2 of 3</b-col>
+            <b-col>3 of 3</b-col>
+        </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { Api } from '@/Api'
+
+export default {
+  name: 'user',
+  data() {
+    return {
+      message: 'none'
+    }
+  },
+  methods: {
+    getMessage() {
+      Api.get('/')
+        .then(response => {
+          this.message = response.data.message
+        })
+        .catch(error => {
+          this.message = error
+        })
+    }
+  }
+}
+</script>
+
+<style>
+.btn_message {
+  margin-bottom: 1em;
+}
+</style>
