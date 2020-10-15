ba3d9c232d7237f24466f09e5ecefdb86a15ae96
Altug <gusaltetal@student.gu.se>
2020-10-15 17:07:56 +0200

    Added budget and category on userpage

---
+++ client/src/components/UserCategoryItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-list-group>
+        <b-list-group-item class="d-flex justify-content-between align-items-center">
+            <p>{{ categoryObject.name }}</p>
+        <b-badge variant="primary" pill>{{ categoryObject.typeExperience }}exp</b-badge>
+        </b-list-group-item>
+    </b-list-group>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'user-category-item',
+  props: ['categoryObject']
+}
+</script>
--- client/src/views/UserDisplay.vue
+++ client/src/views/UserDisplay.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <b-container v-if="loggedIn">
-        <p class="red">{{message}}</p>
         <h1>User</h1>
         <b-row align-h="center">
             <b-col cols="12" sm="6" md="4">
@@ -10,6 +9,8 @@
         </b-row>
     </b-container>
 
+    <b-row align-h="center">
+      <b-col cols="12" sm="6" md="4">
     <div class="accordion" role="tablist">
       <b-card no-body class="mb-1">
         <b-card-header header-tag="header" class="p-1" role="tab">
@@ -18,6 +19,7 @@
         <b-collapse id="accordion-1" accordion="my-accordion" role="tabpanel">
           <b-card-body>
             <b-row align-h="center">
+              <p><b-button v-if="achFlag">{{ messagea }}click me to create one!</b-button></p>
             <b-col cols="12" sm="6" md="4" v-for="achievement in achievements" :key="achievement._id">
                 <user-achievement-item class="items" v-bind:achievementObject="achievement"/>
             </b-col>
@@ -25,7 +27,41 @@
           </b-card-body>
         </b-collapse>
       </b-card>
+
+      <b-card no-body class="mb-1">
+        <b-card-header header-tag="header" class="p-1" role="tab">
+          <b-button block v-b-toggle.accordion-2 variant="info">Budgets</b-button>
+        </b-card-header>
+        <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
+          <b-card-body>
+            <b-row align-h="center">
+              <p><b-button v-if="budFlag">{{ messageb }}click me to create one!</b-button></p>
+            <b-col cols="12" sm="6" md="4" v-for="budget in budgets" :key="budget._id">
+                <user-budget-item class="items" v-bind:budgetObject="budget"/>
+            </b-col>
+        </b-row>
+          </b-card-body>
+        </b-collapse>
+      </b-card>
+
+      <b-card no-body class="mb-1">
+        <b-card-header header-tag="header" class="p-1" role="tab">
+          <b-button block v-b-toggle.accordion-3 variant="info">Other Categories</b-button>
+        </b-card-header>
+        <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
+          <b-card-body>
+            <b-row align-h="center">
+              <p><b-button v-if="catFlag">{{ messagec }}click me to create one!</b-button></p>
+            <b-col cols="12" sm="6" md="4" v-for="category in categories" :key="category._id">
+                <user-category-item class="items" v-bind:categoryObject="category"/>
+            </b-col>
+        </b-row>
+          </b-card-body>
+        </b-collapse>
+      </b-card>
     </div>
+      </b-col>
+    </b-row>
 
   </div>
 </template>
@@ -35,16 +71,22 @@ import { Api } from '@/Api'
 import UserItem from '@/components/UserItem.vue'
 import cookiesC from '../cookies/cookies'
 import UserAchievementItem from '@/components/UserAchievementItem.vue'
+import UserBudgetItem from '@/components/UserBudgetItem.vue'
+import UserCategoryItem from '@/components/UserCategoryItem.vue'
 
 export default {
   name: 'users',
   props: ['loggedIn', 'logging'],
   components: {
     UserItem,
-    UserAchievementItem
+    UserAchievementItem,
+    UserBudgetItem,
+    UserCategoryItem
   },
   mounted() {
     this.getAchievement()
+    this.getBudget()
+    this.getCategory()
     var id = cookiesC.getCookieValue('id')
     Api.get(`/Users/${id}`)
       .then(response => {
@@ -57,10 +99,17 @@ export default {
   },
   data() {
     return {
-      message: '',
+      messagea: '',
+      messageb: '',
+      messagec: '',
       text: '',
       userinfo: {},
-      achievements: []
+      achFlag: false,
+      budFlag: false,
+      catFlag: false,
+      achievements: [],
+      budgets: [],
+      categories: []
     }
   },
   methods: {
@@ -91,8 +140,36 @@ export default {
           this.achievements = response.data
         })
         .catch(error => {
-          this.message = error.message
+          this.achFlag = true
+          error.message = 'You have no achievements yet, '
+          this.messagea = error.message
           this.achievements = []
+        })
+    },
+    getBudget() {
+      var id = cookiesC.getCookieValue('id')
+      Api.get(`/users/${id}/budgets`)
+        .then(response => {
+          this.budgets = response.data
+        })
+        .catch(error => {
+          this.budFlag = true
+          error.message = 'You have no budget yet, '
+          this.messageb = error.message
+          this.budgets = []
+        })
+    },
+    getCategory() {
+      var id = cookiesC.getCookieValue('id')
+      Api.get(`/users/${id}/categories`)
+        .then(response => {
+          this.categories = response.data
+        })
+        .catch(error => {
+          this.catFlag = true
+          error.message = 'You have no other categories yet, '
+          this.messagec = error.message
+          this.budgets = []
         })
     }
   }
