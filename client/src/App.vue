e36a33a1d023cc5d4fd63d2deea402e7674d9e03
Filip <filiple@student.chalmers.se>
2020-10-19 17:53:16 +0200

    minor style changes in template

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class ="main bg-dark" id="app">
     <b-button v-b-toggle.sidebar-1>Pages</b-button>
-    <b-sidebar id="sidebar-1" title="Sidebar" shadow>
+    <b-sidebar id="sidebar-1" title="Menu" bg-variant="dark" :backdrop-variant="variant" backdrop shadow>
       <div class="px-3 py-2">
         <hr class="my-4">
         <p>
@@ -101,8 +101,12 @@ export default {
 }
 .serif {
   font-family: "Times New Roman", Times, serif;
+  color: white;
 }
 #profileLink {
   font-weight: bold;
+}
+#sidebar-1 {
+  background-color: #2c3e50;
 }
 </style>
